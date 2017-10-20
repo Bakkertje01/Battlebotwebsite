63c81d7686907e5427cd7eb68d3755688ecf32dd
root <root@DESKTOP-226HC7T.localdomain>
2017-10-20 17:33:22 +0200

    New files uploaded, socketjs for data conenction

--- include/head.php
+++ include/head.php
@@ -9,6 +9,7 @@
 <!--socket.io libary -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js/socket.js"></script>
 
 <link rel="icon" href="images/favicon.png" type="image/x-icon"/>
 
--- index.php
+++ index.php
@@ -254,27 +254,3 @@ include 'include/navigation.php';
 
 
 </html>
-<!--Test commit-->
-<script>
-    var socket = io.connect('http://localhost:8080');
-    socket.on('connected', function (data) {
-        $("h1").html('Connection established')
-    });
-
-    socket.on('data_received', function(data){
-        console.log(data);
-    });
-
-    socket.on('disconnect', function(data){
-        $("h1").html(data)
-    });
-
-    function close(){
-        socket.emit("close", "");
-        console.info("Close command send...")
-    }
-    function search(){
-        socket.emit("search", "");
-        console.info("Search command send...")
-    }
-</script>
